179294965ddc82bce91448c88178f3c2567975c4
Marco.Antonio <mo27marco@gmail.com>
2024-12-02 15:51:35 -0600

    Ventas-vue

---
+++ src/views/MainNav.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="px-10 py-5 bg-gray-700 flex justify-between absolute top-0 w-full z-10">
+        <div>
+            <h1 class="text-3xl font-black">
+                Ventas
+                <span class="text-green-400">Vue.js</span>
+            </h1>
+        </div>
+
+        <nav>
+
+        </nav>
+
+    </header>
+</template>
---
+++ src/views/ShopView.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import MainNav from './MainNav.vue';
+
+</script>
+
+<template>
+    <h1>Shop View</h1>
+    <div>
+        <MainNav />
+    </div>
+</template>
